df682d51071d8c0b421ed4ba1238f9276b1f7985
João Ferreira Loff <jfloff@gmail.com>
2019-06-05 11:37:43 +0100

    Fix build-all script to use printf instead of echo. Simplify getting script path

--- build-all.sh
+++ build-all.sh
@@ -8,20 +8,21 @@ reset="$(tput sgr0)"
 build_with_status () {
   tag="$1"
 
-  echo -n "Building $tag"
+  printf "${b}Building ${tag} ... "
   docker build . -t $tag &> /dev/null
   if [[ $? -ne 0 ]]; then
-    echo "$bold$red BUILD FAILED$reset"
+    printf "\n${bold}${red}BUILD FAILED${reset}"
     exit 1
   fi
 
   echo 'print("something")' | docker run --rm -i $tag &> /dev/null
   if [[ $? -ne 0 ]]; then
-    echo "$bold$red TEST FAILED$reset"
+    printf "${bold}${red}TEST FAILED${reset}"
     exit 1
   else
-    echo "$bold$green SUCCESS$reset"
+    printf "${bold}${green}SUCCESS${reset}"
   fi
+  printf "\n"
 }
 
 # quiet versions
@@ -29,8 +30,8 @@ pushd() { builtin pushd $1 > /dev/null; }
 popd() { builtin popd > /dev/null; }
 
 # Move to where the script is
-HERE="$(dirname $(readlink -f $0 || realpath $0))"
-cd $HERE
+HERE="$( cd "$(dirname "$0")" ; pwd -P )"
+cd "$HERE"
 
 # Find all the top-level dirs
 for version in $(find -maxdepth 1 -not -name '.*' -type d -printf '%P\n' | sort); do
